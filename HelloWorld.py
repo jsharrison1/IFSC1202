6b0a6525ccff3ff0d0ea009ecbdf6c71af495ca7
jsharrison1 <jsharrison1@ualr.edu>
2022-08-26 15:22:14 +0000

    01.01 Sum of Three Numbers.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,12 @@
-# Prompt for name
-James = input("First Name: ")
-# Add " Hellow " name
-greeting = "Hellow " + James
-# Print greeting
-print(greeting)
+x = 2
+a = 3
+b = 4
+print(x + a + b)
+c = -5
+y = 180
+g = 17
+print(c + y +g)
+t = 2
+d= 0
+k = -6
+print (t + d + k)
